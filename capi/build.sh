bef27c4ddb24a6b77d5254d5dd466f2ea4691cb1
Hazard <hazard9909@gmail.com>
2020-03-07 00:17:43 +0100

    Gitignored generated CAPI header files

--- capi/build.sh
+++ capi/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cmake . -BBUILD -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja -DCMAKE_C_COMPILER='clang' -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_LINKER='ld.lld' && cmake --build BUILD --config RelWithDebInfo
+cmake . -BBUILD -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja -DCMAKE_C_COMPILER='clang-9' -DCMAKE_CXX_COMPILER='clang++-9' -DCMAKE_LINKER='ld.lld-9' && cmake --build BUILD --config RelWithDebInfo
---
+++ capi/gen-server.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+rm -f -r server
+mkdir -p server
+cd server
+
+../../tools/altv-api-tool \
+../../cpp-sdk/SDK.h \
+ \
+-- \
+ \
+-DUNICODE \
+-D_UNICODE \
+-DALT_SERVER \
+-DALT_SERVER_API \
+ \
+-fms-compatibility \
+-fms-extensions \
+-fdelayed-template-parsing \
+ \
+-Wno-deprecated-declarations \
+-Wno-switch-enum \
+-Wno-unused-command-line-argument \
+-Wno-macro-redefined \
+-Wno-inconsistent-missing-override \
+-Wno-deprecated-declarations \
+-Wno-return-type \
+-Wno-switch-enum \
+-Wno-switch \
+-Wno-parentheses \
+-Wno-unused-private-field \
+-Wno-unused-variable \
+-Wno-format \
+-Wno-reorder \
+-Wno-delete-non-virtual-dtor \
+-Wno-microsoft-cast \
+-Wno-unused-function \
+-Wno-unused-lambda-capture \
+-Wno-missing-braces \
+-Wno-unused-local-typedef \
+-Wno-tautological-constant-out-of-range-compare \
+-Wno-delete-incomplete \
+-Wno-c++11-narrowing \
+-Wno-comment \
+ \
+-xc++ \
+-ferror-limit=0 \
+-Wno-pragma-once-outside-header \
+-std=c++17 \
+-Wno-switch-enum \
+-Wno-switch \
+ \
+-fparse-all-comments \
+ \
+-nostdinc \
+-nostdinc++ \
+-I /WINDOWS/VC/Tools/MSVC/14.16.27023/include
