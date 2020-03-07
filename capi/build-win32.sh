da2209c8743e93bb9901e6490f2271bfbcb9eecb
Hazard <hazard9909@gmail.com>
2020-03-07 13:12:48 +0100

    Dont gen capi every build

--- capi/build-win32.sh
+++ capi/build-win32.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "GENERATING CLIENT CODE"
-./gen-client.sh
-echo "GENERATING SERVER CODE"
-./gen-server.sh
+export RC=/usr/bin/llvm-rc-9
+export rc=/usr/bin/llvm-rc-9
+export MT=/usr/bin/llvm-mt-9
+export mt=/usr/bin/llvm-mt-9
 
-echo "BUILDING WRAPPERS"
+echo "BUILDING CAPI"
 cmake . -BBUILD-WIN32 -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja -DCMAKE_TOOLCHAIN_FILE='../cmake/x64_win32.cmake' -DCWIN_LLVM_VERSION=9
 cmake --build BUILD-WIN32 --config RelWithDebInfo
 
--- capi/build.sh
+++ capi/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "GENERATING CLIENT CODE"
-./gen-client.sh
-echo "GENERATING SERVER CODE"
-./gen-server.sh
-
-echo "BUILDING WRAPPERS"
+echo "BUILDING CAPI"
 cmake . -BBUILD -DCMAKE_BUILD_TYPE=RelWithDebInfo -GNinja -DCMAKE_C_COMPILER='clang-9' -DCMAKE_CXX_COMPILER='clang++-9' -DCMAKE_LINKER='ld.lld-9'
 cmake --build BUILD --config RelWithDebInfo
