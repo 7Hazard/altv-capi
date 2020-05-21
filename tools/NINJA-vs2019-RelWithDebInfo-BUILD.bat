41c53562990cf84d7c8d19b06f542e60825be7ce
Hazard <hazard9909@gmail.com>
2020-05-21 03:26:25 +0200

    Added tools for vs2019

---
+++ tools/NINJA-vs2019-RelWithDebInfo-BUILD.bat
@@ -0,0 +1,12 @@
+:: Buildscript for building on windows with Ninja and clang-cl
+:: Copy the script to llvm-project root dir
+
+set "curdir=%cd%"
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall" amd64
+cd /d %curdir%
+
+set CFLAGS=-m64
+set CXXFLAGS=-m64
+
+cmake --build A-BUILD-NINJA-RelWithDebInfo --config RelWithDebInfo --target altv-api-tool
