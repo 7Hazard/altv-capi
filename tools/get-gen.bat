40f4ca3e7032a9621091c704c81625b8aef5d8fe
Hazard <hazard9909@gmail.com>
2020-05-27 02:22:15 +0200

    Improve gen getter for win

--- tools/get-gen.bat
+++ tools/get-gen.bat
@@ -3,6 +3,9 @@
 :: CALL FROM PROJ ROOT DIR | ./tools/get-cppsdk.bat
 
 if exist tools\altv-capi-gen.exe del tools\altv-capi-gen.exe
+if exist tools\altv-capi-gen.exe.tmp del tools\altv-capi-gen.exe.tmp
 echo Fetching latest alt:V CAPI Generator
-powershell -Command "Invoke-WebRequest https://github.com/7Hazard/altv-capi/releases/latest/download/altv-capi-gen.exe -OutFile tools/altv-capi-gen.exe"
+call powershell -Command "Invoke-WebRequest https://github.com/7Hazard/altv-capi/releases/latest/download/altv-capi-gen.exe -OutFile tools/altv-capi-gen.exe.tmp"
+copy tools\altv-capi-gen.exe.tmp tools\altv-capi-gen.exe
+del tools\altv-capi-gen.exe.tmp
 echo Fetched latest alt:V CAPI Generator
