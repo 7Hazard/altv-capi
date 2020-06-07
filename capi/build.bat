498534fcf387083b0166285f31f76aab0abd9536
Hazard <hazard9909@gmail.com>
2020-06-07 20:58:29 +0200

    better finding of vcvarsall

--- capi/build.bat
+++ capi/build.bat
@@ -1,9 +1,32 @@
 :: Buildscript for building on windows with Ninja and clang-cl
 :: Copy the script to llvm-project root dir
 
+@echo off
+
 set "curdir=%cd%"
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall" amd64
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" goto vs2019c
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" goto vs2019e
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" goto vs2017c
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" goto vs2017e
+
+echo COULDNT FIND VS INSTALL
+EXIT /B 1
+
+:vs2019c
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+goto CURDIR
+:vs2019e
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+goto CURDIR
+:vs2017c
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+goto CURDIR
+:vs2017e
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+goto CURDIR
+
+:CURDIR
 cd /d %curdir%
 
 set CFLAGS=-m64
